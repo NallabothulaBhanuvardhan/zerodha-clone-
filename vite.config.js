803df4e6349247f3d078a6e15c6c4ee333b884ca
Nallabothulabhanuvardhan <bvardhan75@gmail.com>
2025-09-27 22:39:16 +0530

    Added base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/zerodha-clone-/',  // must match your repo name
   plugins: [react()],
 })
